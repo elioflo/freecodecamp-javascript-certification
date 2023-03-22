6f9532f14b8254fd1c708f549974d7ca8e97ed4c
Elio Flores <38950204+elioflo@users.noreply.github.com>
2023-03-22 18:27:32 -0300

    WIP: freecodecamp javascrip certification...
    Complete caesar cipher and telephone number validator.

--- projects/caesars_cipher.js
+++ projects/caesars_cipher.js
@@ -1,5 +1,9 @@
 function rot13(str) {
-  return str;
+  return str.split('').map(char => {
+    if('A' <= char && char <= 'Z')
+      return String.fromCharCode(char.charCodeAt(0)+13 > 'Z'.charCodeAt(0)? 'A'.charCodeAt(0) + (char.charCodeAt(0) + 13) % 'Z'.charCodeAt(0) - 1: char.charCodeAt(0) + 13)
+    if('a' <= char && char <= 'z')
+      return String.fromCharCode(char.charCodeAt(0)+13 > 'z'.charCodeAt(0)? 'a'.charCodeAt(0) + (char.charCodeAt(0) + 13) % 'z'.charCodeAt(0) - 1: char.charCodeAt(0) + 13)
+    return char
+  }).join('')
 }
-
-rot13("SERR PBQR PNZC");
